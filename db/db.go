1f32d660fe4a254badd7184549269b411f82419f
up1 <somkiat.p@gmail.com>
2025-01-29 21:45:06 +0700

    Manage data with database

---
+++ db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import "database/sql"
+
+type User struct {
+	ID   int
+	Name string
+}
+
+type DB struct {
+	db *sql.DB
+}
+
+func NewDB(db *sql.DB) *DB {
+	return &DB{db: db}
+}
+
+func (d *DB) FindById(id int) (*User, error) {
+	var user User
+	err := d.db.QueryRow("SELECT id, name FROM users WHERE id = ?", id).Scan(&user.ID, &user.Name)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
